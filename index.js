f36f5bf58dc78fccfe61742918e54be5cae7ad86
woruka onward <worukaonward@gmail.com>
2023-03-25 21:29:25 -0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction () {
+    document.getElementById('demo1').innerHTML = ('Hello Onward!');
+    document.getElementById('demo2').innerHTML = ('How are you?');
+}
+
+var x
+x = 6
+
+document.getElementById('demo4').innerHTML = 'John is ' + (x + 10) + 'years ' + 'old';
